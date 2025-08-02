3cd39f5e2afeed0f9493a26719a2a16f9699dfa7
ribmarciojr <marciojunior2109@gmail.com>
2025-08-02 12:56:03 -0300

    reafactor: reduce api image size from 812MB to 383MB with typescript compile and remove unecessary files

--- skumanagement/api/v1/src/application/usecases/disable-sku/disable-sku.ts
+++ skumanagement/api/v1/src/application/usecases/disable-sku/disable-sku.ts
@@ -4,24 +4,29 @@ import { UpdateSkuStatusUseCase } from "../../../domain/interfaces/UpdateSkuStat
 import { ISkuRepository } from "../../../domain/repositories/skuRepositoryInterface";
 import { SkuStateMachine } from "../../../domain/states/SkuStateMachine";
 import { SkuStatusMapper } from "../../../infra/mappers/SkuStatusMapper";
+import { SkuStatusEnum as PrismaStatusEnum } from "@prisma/client";
 
 export class DisableSkuUseCase implements UpdateSkuStatusUseCase {
-    constructor(private readonly skuRepository: ISkuRepository) { }
+  constructor(private readonly skuRepository: ISkuRepository) {}
 
-    async execute(skuId: number, status: string): Promise<void> {
-        const sku = await this.skuRepository.findById(skuId);
+  async execute(skuId: number, status: string): Promise<void> {
+    const sku = await this.skuRepository.findById(skuId);
 
-        if (!sku) {
-            throw new UnprocessableEntity("SKU not found.");
-        }
-
-        const domainStatus = SkuStatusMapper.toDomain(sku.status.status);
-
-        const skuStateMachine = new SkuStateMachine(domainStatus);
-
-
-        skuStateMachine.transitionTo(SkuStatusEnum.DISABLED);
-
-        await this.skuRepository.updateStatus(skuId, SkuStatusEnum.DISABLED);
+    if (!sku) {
+      throw new UnprocessableEntity("SKU not found.");
     }
-}       
+
+    const domainStatus = SkuStatusMapper.toDomain(
+      sku.status?.status as PrismaStatusEnum
+    );
+
+    const skuStateMachine = new SkuStateMachine(domainStatus);
+
+    skuStateMachine.transitionTo(SkuStatusEnum.DISABLED);
+
+    await this.skuRepository.updateStatus({
+      skuId,
+      askedStatus: SkuStatusEnum.DISABLED,
+    });
+  }
+}
